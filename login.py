b20267387516b1d55b8470a5bf9efd655ab8422f
AJFintastic <akshar@fintasticdata.com>
2025-02-19 10:37:06 +0200

    Update login.py

--- login.py
+++ login.py
@@ -1,26 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 10 12:12:38 2025
-
-@author: user
-"""
-
-# login.py
-
 import streamlit as st
 from services import check_credentials
 
-def show_login():
-    """
-    Renders the login section. Prompts the user for a Client ID (used as password).
-    """
-    st.subheader("Login")
+def login_callback():
+    client_id_input = st.session_state.get("client_id_input")
+    if check_credentials(client_id_input):
+        st.session_state.logged_in = True
+        st.session_state.client_id = client_id_input
+        st.success("Login successful!")
+        st.rerun()  # Refresh the app immediately
+    else:
+        st.error("Invalid Client ID. Please try again.")
 
-    client_id_input = st.text_input("Enter your Client ID (password)", type="password")
-    if st.button("Login"):
-        if check_credentials(client_id_input):
-            st.session_state.logged_in = True
-            st.session_state.client_id = client_id_input
-            st.success("Login successful!")
-        else:
-            st.error("Invalid Client ID. Please try again.")
+def show_login():
+    st.subheader("Login")
+    st.text_input("Enter your Client ID (password)", key="client_id_input", type="password")
+    st.button("Login", on_click=login_callback)
